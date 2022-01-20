ce8ff604c81fce676f2d30facbd416192e82106e
Eirik Jensen <eirjen15@o365.mrvgs.no>
2022-01-20 11:27:10 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$protocol = $_SERVER['SERVER_PROTOCOL'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$port = $_SERVER['REMOTE_PORT'];
+$agent = $_SERVER['HTTP_USER_AGENT'];
+$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+ 
+$fh = fopen('log.txt', 'a');
+fwrite($fh, 'IP Address: '."".$ip ."\n");
+fwrite($fh, 'Hostname: '."".$hostname ."\n");
+fwrite($fh, 'Port Number: '."".$port ."\n");
+fwrite($fh, 'User Agent: '."".$agent ."\n");
+fclose($fh);
+?>
